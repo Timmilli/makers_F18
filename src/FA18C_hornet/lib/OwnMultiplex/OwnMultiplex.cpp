1ef46db744dd905777f3cc2e519a1a6485f1c09c
Guenael Roger <guenael.roger@enseirb-matmeca.fr>
2025-05-10 11:57:27 +0200

    feat: changed the way potentiometer's pcf's pins are accessed to

--- src/FA18C_hornet/lib/OwnMultiplex/OwnMultiplex.cpp
+++ src/FA18C_hornet/lib/OwnMultiplex/OwnMultiplex.cpp
@@ -2,65 +2,63 @@
 #include "Arduino.h"
 #include "Constants.h"
 
-float getPotentiometerValue(OwnPCF *oPCF, uint8_t index) {
+float getPotentiometerValue(OwnPCF *oPCF, uint8_t index, unsigned int msb,
+                            unsigned int midsb, unsigned int lsb) {
   float res = 0.0;
   switch (index) {
   case (0):
-    oPCF->deactivatePin(FIRST_POTENTIOMETER_PIN);
-    oPCF->deactivatePin(SECOND_POTENTIOMETER_PIN);
-    oPCF->deactivatePin(THIRD_POTENTIOMETER_PIN);
+    oPCF->deactivatePin(msb);
+    oPCF->deactivatePin(midsb);
+    oPCF->deactivatePin(lsb);
     res = analogRead(ANALOG_READ_ADDR);
   case (1):
-    oPCF->activatePin(THIRD_POTENTIOMETER_PIN);
+    oPCF->activatePin(lsb);
     res = analogRead(ANALOG_READ_ADDR);
-    oPCF->deactivatePin(THIRD_POTENTIOMETER_PIN);
+    oPCF->deactivatePin(lsb);
     break;
   case (2):
-    oPCF->activatePin(SECOND_POTENTIOMETER_PIN);
+    oPCF->activatePin(midsb);
     res = analogRead(ANALOG_READ_ADDR);
-    oPCF->deactivatePin(SECOND_POTENTIOMETER_PIN);
+    oPCF->deactivatePin(midsb);
     break;
   case (3):
-    oPCF->activatePin(SECOND_POTENTIOMETER_PIN);
-    oPCF->activatePin(THIRD_POTENTIOMETER_PIN);
+    oPCF->activatePin(midsb);
+    oPCF->activatePin(lsb);
     res = analogRead(ANALOG_READ_ADDR);
-    oPCF->deactivatePin(SECOND_POTENTIOMETER_PIN);
-    oPCF->deactivatePin(THIRD_POTENTIOMETER_PIN);
+    oPCF->deactivatePin(midsb);
+    oPCF->deactivatePin(lsb);
     break;
   case (4):
-    oPCF->activatePin(FIRST_POTENTIOMETER_PIN);
+    oPCF->activatePin(msb);
     res = analogRead(ANALOG_READ_ADDR);
-    oPCF->deactivatePin(FIRST_POTENTIOMETER_PIN);
+    oPCF->deactivatePin(msb);
     break;
   case (5):
-    oPCF->activatePin(FIRST_POTENTIOMETER_PIN);
-    oPCF->activatePin(THIRD_POTENTIOMETER_PIN);
+    oPCF->activatePin(msb);
+    oPCF->activatePin(lsb);
     res = analogRead(ANALOG_READ_ADDR);
-    oPCF->deactivatePin(FIRST_POTENTIOMETER_PIN);
-    oPCF->deactivatePin(THIRD_POTENTIOMETER_PIN);
-    break;
-  case (6):
-    oPCF->activatePin(FIRST_POTENTIOMETER_PIN);
-    oPCF->activatePin(SECOND_POTENTIOMETER_PIN);
     res = analogRead(ANALOG_READ_ADDR);
-    oPCF->deactivatePin(FIRST_POTENTIOMETER_PIN);
-    oPCF->deactivatePin(SECOND_POTENTIOMETER_PIN);
+    oPCF->deactivatePin(msb);
+    oPCF->deactivatePin(midsb);
     break;
   case (7):
-    oPCF->activatePin(FIRST_POTENTIOMETER_PIN);
-    oPCF->activatePin(SECOND_POTENTIOMETER_PIN);
-    oPCF->activatePin(THIRD_POTENTIOMETER_PIN);
+    oPCF->activatePin(msb);
+    oPCF->activatePin(midsb);
+    oPCF->activatePin(lsb);
     res = analogRead(ANALOG_READ_ADDR);
-    oPCF->deactivatePin(FIRST_POTENTIOMETER_PIN);
-    oPCF->deactivatePin(SECOND_POTENTIOMETER_PIN);
-    oPCF->deactivatePin(THIRD_POTENTIOMETER_PIN);
+    oPCF->deactivatePin(msb);
+    oPCF->deactivatePin(midsb);
+    oPCF->deactivatePin(lsb);
     break;
   }
   return res;
 }
 
-OwnMultiplex::OwnMultiplex(OwnPCF *oPCF, byte pin_enable, byte pin_read)
-    : _oPCF(oPCF), _pin_enable(pin_enable), _pin_read(pin_read) {
+OwnMultiplex::OwnMultiplex(OwnPCF *oPCF, unsigned int MSBit_pin,
+                           unsigned int midBit_pin, unsigned int LSBit_pin,
+                           byte pin_enable, byte pin_read)
+    : _oPCF(oPCF), _msb(MSBit_pin), _midsb(midBit_pin), _lsb(LSBit_pin),
+      _pin_enable(pin_enable), _pin_read(pin_read) {
   for (uint8_t i = 0; i < 8; i++) {
     _states[i] = 0.0;
   }
@@ -81,7 +79,7 @@ void OwnMultiplex::startMultiplex() {
 
 void OwnMultiplex::update() {
   for (uint8_t i = 0; i < 8; i++) {
-    _states[i] = getPotentiometerValue(_oPCF, i);
+    _states[i] = getPotentiometerValue(_oPCF, i, _msb, _midsb, _lsb);
   }
 }
 
--- src/FA18C_hornet/lib/OwnMultiplex/OwnMultiplex.hpp
+++ src/FA18C_hornet/lib/OwnMultiplex/OwnMultiplex.hpp
@@ -7,12 +7,15 @@
 class OwnMultiplex {
 private:
   OwnPCF *_oPCF;
+  // Most, mid and least significant bits
+  unsigned int _msb, _midsb, _lsb;
   byte _pin_enable;
   byte _pin_read;
   float _states[8];
 
 public:
-  OwnMultiplex(OwnPCF *oPCF, byte pin_enable, byte pin_read);
+  OwnMultiplex(OwnPCF *oPCF, unsigned int MSBit_pin, unsigned int midBit_pin,
+               unsigned int LSBit_pin, byte pin_enable, byte pin_read);
   void startMultiplex(); // TODO check cuz not sure if this is to be kept
   void update();
   int getState(unsigned int index);
